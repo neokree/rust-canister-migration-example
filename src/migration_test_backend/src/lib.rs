91710cb106529c3b4897fc3e9c054822bc335078
NeoKree <neokree@gmail.com>
2022-12-14 20:23:24 +0100

    Add pre_upgrade and post_upgrade methods

--- src/migration_test_backend/src/lib.rs
+++ src/migration_test_backend/src/lib.rs
@@ -1,30 +1,35 @@
-use ic_cdk::export::candid;
+use ic_cdk::{export::candid, storage};
 use ic_cdk_macros::*;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
 
-static mut COUNTER: Option<candid::Nat> = None;
-
-#[init]
-fn init() {
-    unsafe {
-        COUNTER = Some(candid::Nat::from(0));
-    }
+lazy_static! {
+    static ref COUNTER: Mutex<candid::Nat> = Mutex::new(candid::Nat::from(0));
 }
 
 #[update]
 fn increment() {
-    unsafe {
-        COUNTER.as_mut().unwrap().0 += 1u64;
-    }
+    let mut counter = COUNTER.lock().unwrap();
+    *counter += 1u64;
 }
 
 #[query]
 fn get() -> candid::Nat {
-    unsafe { COUNTER.as_mut().unwrap().clone() }
+    COUNTER.lock().unwrap().clone()
 }
 
-#[update]
-fn set(input: candid::Nat) {
-    unsafe {
-        COUNTER.as_mut().unwrap().0 = input.0;
-    }
+#[pre_upgrade]
+fn pre_upgrade() {
+    ic_cdk::println!("pre_upgrade IN");
+    let counter = COUNTER.lock().unwrap();
+    _ = storage::stable_save((counter.clone(),));
+    ic_cdk::println!("pre_upgrade OUT");
+}
+
+#[post_upgrade]
+fn post_upgrade() {
+    ic_cdk::println!("post_upgrade IN");
+    let (counter,): (candid::Nat,) = storage::stable_restore().unwrap_or((candid::Nat::from(0),));
+    *COUNTER.lock().unwrap() = counter;
+    ic_cdk::println!("post_upgrade OUT");
 }
